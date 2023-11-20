2c8c37748ee169b74f893572735ece2613c75a35
entriphy <1747088+entriphy@users.noreply.github.com>
2023-11-19 22:31:43 -0800

    Update Python file with extra credit progress

--- project_1.py
+++ project_1.py
@@ -10,36 +10,37 @@ def parse_number(token: str) -> bool:
                     state = 1
                 elif c == '0':
                     state = 3
+                elif c == '.':
+                    state = 1
                 else:
                     return False
             case 1:  # Decimal state
                 if '0' <= c <= '9':
                     state = 1
+                elif c == ".":
+                    state = 4
+                elif c in ['e', 'E']:
+                    state = 5
                 elif c == "_":
                     state = 8
                 else:
                     return False
-            case 2:  # Accept state
+            case 2 | 4 | 5:  # Accept states
                 return True
             case 3:  # Octal/hexadecimal transition state
                 if c in ['o', 'O']:
-                    state = 4
+                    state = 6
                 elif c in ['x', 'X']:
                     state = 6
+                elif c == ".":
+                    state = 4
                 else:
                     return False
-            case 4 | 5:  # Octal state
-                if '0' <= c <= '7':
-                    state = 5
-                elif c == "_":
-                    state = 9
-                else:
-                    return False
-            case 6 | 7:  # Hexadecimal state
-                if '0' <= c <= '9' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
+            case 6 | 7:  # Octal/hexadecimal states
+                if '0' <= c <= '7' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
                     state = 7
                 elif c == "_":
-                    state = 10
+                    state = 9
                 else:
                     return False
             case 8:  # Decimal underscore state
@@ -47,19 +48,14 @@ def parse_number(token: str) -> bool:
                     state = 1
                 else:
                     return False
-            case 9:  # Octal underscore state
-                if '0' <= c <= '7':
-                    state = 5
-                else:
-                    return False
-            case 10:  # Hexadecimal underscore state
-                if '0' <= c <= '9' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
+            case 9:  # Octal/hexadecimal underscore state
+                if '0' <= c <= '7' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
                     state = 7
                 else:
                     return False
 
-    # Epsilon transition to state 2 (accept state)
-    if state in [1, 5, 7]:
+    # Epsilon transition to accept states
+    if state in [1, 4, 5, 7]:
         return True
     else:
         return False
@@ -76,6 +72,24 @@ TEST_DATA = [
     ("0xdeadbeef", True),
     ("100_000_000_000", True),
 
+    # Floating-point literal examples
+    ("3.14", True),
+    ("0.5", True),
+    (".5", True),
+    ("0.", True),
+    ("000.", True),
+    ("1.23e-4", True),
+    ("1e10", True),
+    ("1e1_0", True),
+    ("1e1__0", False),
+    ("1_0e10", True),
+    ("1_0e-10", True),
+    ("1_0e+10", True),
+    ("1_0e+1_0", True),
+    ("1.00e+10", True),
+    ("1.00e+", False),
+    ("1.00e", False),
+
     # Other cases
     ("_", False),
     ("1__0", False),
@@ -88,6 +102,7 @@ TEST_DATA = [
     ("0x0_", False),
     ("0x_0", True),
     ("0xg", False),
+    ("00x00", False),
 
     ("2o0", False),
     ("0o", False),
@@ -99,7 +114,7 @@ TEST_DATA = [
 ]
 
 def main():
-    if sys.argv[-1] == "test":  # Test mode
+    if sys.argv[-1] != "test":  # Test mode
         # Verify test data
         for string, expected in TEST_DATA:
             try:
@@ -121,7 +136,6 @@ def main():
                 print(f"{input_num} is {'a' if result else 'not a'} valid number.")
             except KeyboardInterrupt:
                 break
-
 
 if __name__ == "__main__":
     main()
