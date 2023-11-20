f73da6e1bf59e299e18356f53950cd867944addc
entriphy <1747088+entriphy@users.noreply.github.com>
2023-11-19 22:55:42 -0800

    Implement new NFA in Python

--- project_1.py
+++ project_1.py
@@ -8,39 +8,53 @@ def parse_number(token: str) -> bool:
             case 0:  # Initial state
                 if '1' <= c <= '9':
                     state = 1
+                elif c == ".":
+                    state = 2
                 elif c == '0':
                     state = 3
-                elif c == '.':
-                    state = 1
                 else:
                     return False
             case 1:  # Decimal state
                 if '0' <= c <= '9':
                     state = 1
-                elif c == ".":
-                    state = 4
-                elif c in ['e', 'E']:
-                    state = 5
                 elif c == "_":
                     state = 8
+                elif c == ".":
+                    state = 11
+                elif c in ["e", "E"]:
+                    state = 12
                 else:
                     return False
-            case 2 | 4 | 5:  # Accept states
-                return True
-            case 3:  # Octal/hexadecimal transition state
+            case 2:  # Point transition state
+                if '0' <= c <= '9':
+                    state = 11
+                else:
+                    return False
+            case 3:  # Octal/hexadecimal transition & "0" state
                 if c in ['o', 'O']:
-                    state = 6
+                    state = 4
                 elif c in ['x', 'X']:
                     state = 6
                 elif c == ".":
-                    state = 4
+                    state = 11
+                elif c in ['e', 'E']:
+                    state = 12
+                elif '0' <= c <= '9':
+                    state = 15
                 else:
                     return False
-            case 6 | 7:  # Octal/hexadecimal states
-                if '0' <= c <= '7' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
-                    state = 7
+            case 4 | 5:  # Octal state
+                if '0' <= c <= '7':
+                    state = 5
                 elif c == "_":
                     state = 9
+                else:
+                    return False
+            case 6 | 7:  # Hexadecimal state
+                if '0' <= c <= '9' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
+                    state = 7
+                elif c == "_":
+                    state = 10
                 else:
                     return False
             case 8:  # Decimal underscore state
@@ -48,17 +62,70 @@ def parse_number(token: str) -> bool:
                     state = 1
                 else:
                     return False
-            case 9:  # Octal/hexadecimal underscore state
-                if '0' <= c <= '7' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
+            case 9:  # Octal underscore state
+                if '0' <= c <= '7':
+                    state = 5
+                else:
+                    return False
+            case 10:  # Hexadecimal underscore state
+                if '0' <= c <= '9' or 'A' <= c <= 'F' or 'a' <= c <= 'f':
                     state = 7
                 else:
                     return False
-
-    # Epsilon transition to accept states
-    if state in [1, 4, 5, 7]:
+            case 11:  # Fractional state
+                if '0' <= c <= '9':
+                    state = 11
+                elif c in ['e', 'E']:
+                    state = 12
+                elif c == "_":
+                    state = 16
+                else:
+                    return False
+            case 12:  # Exponent transition state
+                if '0' <= c <= '9':
+                    state = 13
+                elif c in ['-', '+']:
+                    state = 14
+                else:
+                    return False
+            case 13:  # Exponent state
+                if '0' <= c <= '9':
+                    state = 13
+                elif c == "_":
+                    state = 14
+                else:
+                    return False
+            case 14:  # Exponent underscore/sign state
+                if '0' <= c <= '9':
+                    state = 13
+                else:
+                    return False
+            case 15:  # Point state
+                if '0' <= c <= '9':
+                    state = 15
+                elif c == ".":
+                    state = 11
+                elif c == "_":
+                    state = 17
+                else:
+                    return False
+            case 16:  # Fractional underscore state
+                if '0' <= c <= '9':
+                    state = 11
+                else:
+                    return False
+            case 17:  # Point underscore state
+                if '0' <= c <= '9':
+                    state = 15
+                else:
+                    return False
+    
+    # End of string; return true if state is an accept state
+    if state in [1, 3, 5, 7, 11, 13]:
         return True
     else:
         return False
+
 
 # (string, expected)
 TEST_DATA = [
@@ -72,11 +139,42 @@ TEST_DATA = [
     ("0xdeadbeef", True),
     ("100_000_000_000", True),
 
-    # Floating-point literal examples
+    # Floating point literal examples from Python documentation
     ("3.14", True),
-    ("0.5", True),
-    (".5", True),
-    ("0.", True),
+    ("10.", True),
+    (".001", True),
+    ("1e100", True),
+    ("3.14e-10", True),
+    ("0e0", True),
+    ("3.14_15_93", True),
+    
+    # Decimal cases
+    ("", False),
+    ("0", True),
+    ("00", False),
+    ("1_0_0", True),
+    ("_", False),
+    ("1__0", False),
+    ("_0", False),
+
+    # Octal cases
+    ("2o0", False),
+    ("0o", False),
+    ("0o0", True),
+    ("0o0_", False),
+    ("0o_0", True),
+    ("0og", False),
+    ("0o8", False),
+
+    # Hexadecimal cases
+    ("2x0", False),
+    ("0x", False),
+    ("0x0", True),
+    ("0x0_", False),
+    ("0x_0", True),
+    ("0xg", False),
+
+    # Floating point cases
     ("000.", True),
     ("1.23e-4", True),
     ("1e10", True),
@@ -89,28 +187,8 @@ TEST_DATA = [
     ("1.00e+10", True),
     ("1.00e+", False),
     ("1.00e", False),
-
-    # Other cases
-    ("_", False),
-    ("1__0", False),
-    ("1_0", True),
-    ("_0", False),
-
-    ("2x0", False),
-    ("0x", False),
-    ("0x0", True),
-    ("0x0_", False),
-    ("0x_0", True),
-    ("0xg", False),
-    ("00x00", False),
-
-    ("2o0", False),
-    ("0o", False),
-    ("0o0", True),
-    ("0o0_", False),
-    ("0o_0", True),
-    ("0og", False),
-    ("0o8", False),
+    ("001", False),
+    ("3_14.15", True)
 ]
 
 def main():
@@ -120,13 +198,14 @@ def main():
             try:
                 eval(string)
             except:
-                if expected:
+                if expected and string != "":
                     print(f"Invalid number in test data: {string}")
                     exit(1)
         
         # Run tests
         for string, expected in TEST_DATA:
-            print(f"{string}: {'PASS' if parse_number(string) == expected else 'FAIL'}")
+            result = parse_number(string)
+            print(f"{'PASS' if result == expected else '***FAIL***'}: {string} (Result: {result}, Expected: {expected})")
     else:  # Manual input mode
         while True:
             print("Press Ctrl + C to exit.")
@@ -134,8 +213,10 @@ def main():
                 input_num = input("Enter a number: ")
                 result = parse_number(input_num)
                 print(f"{input_num} is {'a' if result else 'not a'} valid number.")
+                print()
             except KeyboardInterrupt:
                 break
+
 
 if __name__ == "__main__":
     main()
